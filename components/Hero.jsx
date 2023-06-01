1a73f18b7b7cf98d38a18b8376a11b65158d8518
sarthak-simplifii <sarthak@simplifii.com>
2023-06-01 09:01:11 +0530

    updated heading

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -21,17 +21,17 @@ const Hero = () => {
         </div> */}
 
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[60px] text-[52px] text-white ss:leading-[90.8px] leading-[70px]">
+            Real Time <br className="sm:block hidden" />{" "}
+            <span className="text-gradient ss:text-[68px]">Payments</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[66px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Teaching Platform.
+        <h1 className="font-poppins font-semibold ss:text-[60px] text-[52px] text-white ss:leading-[90.8px] leading-[70px] w-full">
+           For Online Lectures.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Embrace the future of online education with our Web3.0 teaching
